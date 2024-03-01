6b6a477ee096abdd41a1edcfba1b5d1a622786e3
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2024-03-02 03:44:04 +0400

    + Game class sketching

--- src/Game.php
+++ src/Game.php
@@ -2,7 +2,37 @@
 
 namespace App;
 
+/**
+ * This class represents the game and handles
+ * its overall flow.
+ */
 class Game
 {
+    public function __construct(
+        private CommandLine $commandLine
+    )
+    {
+    }
 
+    /**
+     * Runs the game by executing its different stages.
+     */
+    public function run()
+    {
+        // welcome
+        $this->commandLine->displayWelcomeMessage();
+
+        // load vehicles
+        $vehicles = $this->loadVehicles();
+
+        // players
+        $players = $this->selectVehicles($vehicles);
+
+        // race
+        $race = new Race($players);
+        $race->run();
+
+        // results
+        $this->displayResults($race);
+    }
 }
