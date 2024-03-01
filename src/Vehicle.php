1711207cd9a1ebc9fd278633c0fbf993cedd8a06
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2024-03-02 03:44:14 +0400

    + vehicle with getters

--- src/Vehicle.php
+++ src/Vehicle.php
@@ -2,7 +2,37 @@
 
 namespace App;
 
+/**
+ * This class represents a vehicle with its properties
+ */
 class Vehicle
 {
+    public function __construct(
+        private string $name,
+        private int $maxSpeed,
+        private int $unit
+    )
+    {
+    }
 
+    /**
+     * ---------------------------------------------------
+     * --------------------  Getters ---------------------
+     * ---------------------------------------------------
+     */
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getMaxSpeed(): int
+    {
+        return $this->maxSpeed;
+    }
+
+    public function getUnit(): int
+    {
+        return $this->unit;
+    }
 }
