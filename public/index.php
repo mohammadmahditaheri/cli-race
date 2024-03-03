682f6416755d953fd42e019cba9451ec4bacf5b0
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2024-03-04 03:58:36 +0400

    -+ pass the other dependency

--- public/index.php
+++ public/index.php
@@ -3,13 +3,17 @@ error_reporting(E_ALL);
 
 use App\Data\Repositories\VehicleRepository as Repo;
 use App\Game;
+use App\Presentation\UI;
 
 define('BASE_PATH', dirname(__DIR__));
 
 require_once BASE_PATH . '/vendor/autoload.php';
 
 
-$game = new Game(new Repo());
+$game = new Game(
+    new Repo(),
+    new UI()
+);
 $game->run();
 
 
